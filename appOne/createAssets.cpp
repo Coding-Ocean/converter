36ff7cc6a5a571678f8c715270926aab75a05de1
Coding-Ocean <eijo524@gmail.com>
2022-08-19 17:14:06 +0900

    tidy assets

--- appOne/createAssets.cpp
+++ appOne/createAssets.cpp
@@ -1,28 +1,34 @@
+//Releaseモードの時は変換を行わない
 #if( _DEBUG  )
-#include "CONVERTER/CONVERT_MA.h"
-#include "CONVERTER/fileUtil.h"
+    #include "CONVERTER/CONVERT_MA.h"
+    #include "CONVERTER/fileUtil.h"
 #endif
 
 void createAssets() 
 {
 #if( _DEBUG )
     CONVERT_MA convertMa;
+    //階層モデル
+    convertMa.fbxToTree(
+        "AssetsOrigin/tank/tank.fbx", //入力ファイル名
+        "AssetsOrigin/tank/tank.txt", //出力ファイル名
+        "Assets/textures/tank/"); //テクスチャ保存フォルダ名
+    //アニメーション
+    convertMa.fbxToAnim(
+        "AssetsOrigin/tank/tank.fbx", //入力ファイル名
+        "AssetsOrigin/tank/tank_move.txt");//出力。拡張子をとった名前がアニメーション名となる。
+
+    //階層なしのモデル(BATCH)でもfbxToTreeで変換
     convertMa.fbxToTree(
         "AssetsOrigin/saikoro/saikoro.fbx", 
         "AssetsOrigin/saikoro/saikoro.txt", 
         "Assets/textures/saikoro/");
-    convertMa.fbxToTree(
-        "AssetsOrigin/test/test.fbx", 
-        "AssetsOrigin/test/test.txt", 
-        "Assets/textures/test/");
-    convertMa.fbxToAnim(
-        "AssetsOrigin/test/test.fbx", 
-        "AssetsOrigin/test/test_move.txt");
 
+    //上記のデータをAssets.txtにまとめる
     const char* assetsFileName = "Assets/Assets.txt";
     delFile(assetsFileName);
     addFile(assetsFileName, "AssetsOrigin/saikoro/saikoro.txt");
-    addFile(assetsFileName, "AssetsOrigin/test/test.txt");
-    addFile(assetsFileName, "AssetsOrigin/test/test_move.txt");
+    addFile(assetsFileName, "AssetsOrigin/tank/tank.txt");
+    addFile(assetsFileName, "AssetsOrigin/tank/tank_move.txt");
 #endif
 }
--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,83 +1,71 @@
-#define _
-#ifdef _
 #include "libOne.h"
 #include "CONTAINER/CONTAINER.h"
 #include "CONTAINER/BATCH.h"
 #include "CONTAINER/TREE.h"
 #include "SHADER/LAMBERT.h"
 #include "createAssets.h"
-void gmain() {
+
+void gmain() 
+{
     window(1920, 1080, full);
     hideCursor();
     
-    int img = loadImage("assets\\images\\gorilla.png");
-    
     createAssets();
-
-    CONTAINER* c = new CONTAINER("assets\\Assets.txt");
-    BATCH* b = c->batch("saikoro");
-    TREE* t = new TREE(c->treeOrigin("test"));
-    ANIMATION* Animation1 = c->animation("test_move");
-    t->setAnimation(Animation1);
+    //load assets
+    CONTAINER* c = new CONTAINER("assets/Assets.txt");
+    //set assets
+    BATCH* batch = c->batch("saikoro");
+    TREE* tree = new TREE(c->treeOrigin("tank"));
+    ANIMATION* anim = c->animation("tank_move");
+    tree->setAnimation(anim);
     float rad = 0;
-    float radB = 0;
-
+    //create shader
     SHADER* shader = new LAMBERT;
     MATRIX world, view, proj;
-    VECTOR camPos(0, 1, 7), lookat(0, 1, 0), up(0, 1, 0);
-    view.camera(camPos, lookat, up);
+    VECTOR camPos(0, 0, 7), lookat(0, 0, 0), up(0, 1, 0);
     proj.pers(3.14f / 4, width / height, 1, 100);
     VECTOR lightPos(0.5f, 0.7f, 1);
-    float ambient = 0.0f;
+    float ambient = 0.2f;
     COLOR diffuse(1, 1, 1, 1);
+    float camRadX = 0.3f;
+    float camRadY = 0.6f;
+    float camRadius = 5;
 
-    while (notQuit) {
-        if (isPress(KEY_D))rad += 0.017f;
-        if (isPress(KEY_A))rad -= 0.017f;
-
-        radB += 0.017f;
-
-        clear();
-        print(rad);
-
-        //3D Graphics------------------------------------------------
+    while (notQuit) 
+    {
+        clear(160,128,128);
+        //control camera
+        if (isPress(KEY_J))camRadY -= 0.005f;
+        if (isPress(KEY_L))camRadY += 0.005f;
+        if (isPress(KEY_I))camRadX += 0.005f;
+        if (isPress(KEY_K))camRadX -= 0.005f;
+        if (isPress(KEY_U))camRadius -= 0.005f;
+        if (isPress(KEY_O))camRadius += 0.005f;
+        camPos.x = Sin(camRadY) * Cos(camRadX) * camRadius;
+        camPos.y = Sin(camRadX) * camRadius;
+        camPos.z = Cos(camRadY) * Cos(camRadX) * camRadius;
+        view.camera(camPos, lookat, up);
+        //set shader
         shader->setProjView(proj * view);
         shader->setLightPosition(lightPos);
         shader->setLightAmbient(ambient);
         shader->setDiffuse(diffuse);
-
+        //batch model
         world.identity();
-        world.mulTranslate(2, 0, 0);
-        world.mulRotateY(radB);
-        world.mulRotateZ(radB*0.3f);
-        world.mulScaling(0.5f, 0.5f, 0.5f);
-        b->draw(shader, world);
-
-        world.identity();
+        world.mulTranslate(0, 0, 0);
         world.mulRotateY(rad);
-        t->update(world);
-        t->draw(shader);
-
-        //2D Graphics------------------------------------------------
-        strokeWeight(10);
-
-        fill(255, 0, 0);
-        rect(0, 100, 100, 100);
-        rect(100, 100, 100, 100);
-        rect(200, 100, 100, 100);
-
-        image(img, 200, 100, 0, 0.18f);
-
-        fill(0, 0, 255);
-        circle(350, 150, 100);
-
-        fill(255);
-        textSize(100);
-        text("Œ¶‘z", 0, 200);
-
-        line(0, 220, 400, 220);
+        world.mulRotateZ(rad * 0.3f);
+        world.mulScaling(0.2f, 0.2f, 0.2f);
+        rad += 0.08f;
+        batch->draw(shader, world);
+        //tree model
+        world.identity();
+        world.mulTranslate(0, 0, 0);
+        tree->update(world);
+        tree->draw(shader);
     }
-    delete t;
+
+    delete shader;
+    delete tree;
     delete c;
 }
-#endif
